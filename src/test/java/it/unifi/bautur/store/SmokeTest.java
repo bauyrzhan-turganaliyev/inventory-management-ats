e23bfdfe99c64270777a5de34215a5a92d03db2d
bauyrzhan-turganaliyev <bauyrzhan.turganaliyev@gmail.com>
2026-08-11 20:26:57 +0200

    Add Coveralls and SonarCloud plugins

--- src/test/java/it/unifi/bautur/store/SmokeTest.java
+++ src/test/java/it/unifi/bautur/store/SmokeTest.java
@@ -1,6 +1,11 @@
+package it.unifi.bautur.store;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+
 class SmokeTest {
+
     @Test
     void buildIsAlive() {
         assertThat(true).isTrue();
