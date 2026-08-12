633962adebee8a4b70f638703b106af241602c49
bauyrzhan-turganaliyev <bauyrzhan.turganaliyev@gmail.com>
2026-08-12 19:40:14 +0200

    Implement Category with name

---
+++ src/main/java/it/unifi/bautur/store/model/Category.java
@@ -0,0 +1,15 @@
+package it.unifi.bautur.store.model;
+
+public class Category {
+
+	private final String name;
+
+	public Category(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
