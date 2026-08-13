3369a0259b750a7d6134a48514ece772ddc5cf81
bauyrzhan-turganaliyev <bauyrzhan.turganaliyev@gmail.com>
2026-08-13 11:12:18 +0200

    Validate Category name in constructor

--- src/main/java/it/unifi/bautur/store/model/Category.java
+++ src/main/java/it/unifi/bautur/store/model/Category.java
@@ -5,7 +5,19 @@ public class Category {
 	private final String name;
 
 	public Category(String name) {
+		validate(name);
+		
 		this.name = name;
+	}
+	
+	private void validate(String name) {
+		if (name == null) {
+			throw new IllegalArgumentException("Category name cannot be null");
+		}
+
+		if (name.trim().isEmpty()) {
+			throw new IllegalArgumentException("Category name cannot be empty");
+		}
 	}
 
 	public String getName() {
